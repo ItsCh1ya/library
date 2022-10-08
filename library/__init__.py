b9a65606293d0197dc4807bac811477667ea862f
Chiya <chiya@yandex.ru>
2022-10-08 13:59:14 +0300

    db: get_all_books

--- library/__init__.py
+++ library/__init__.py
@@ -4,6 +4,8 @@ from library.backend.db import create_connection
 
 app = Flask(__name__, static_folder="static/assets", template_folder="static")
 connection = create_connection("db.sqlite")
+cur = connection.cursor()
+cur.execute("create table if not exists books(id, title, link) ")
 
 from library.views import *
 from library.backend.api import *
--- library/backend/api.py
+++ library/backend/api.py
@@ -1,6 +1,14 @@
 from library import app
+from library.backend.db import create_connection
 from flask import request, jsonify
 
 @app.route("/api/save_book")
 def api_save_book():
     requested_json = request.json
+
+@app.route("/api/get_all_books")
+def api_get_all_books():
+    connection = create_connection("db.sqlite")
+    cur = connection.cursor()
+    all_books = cur.execute("select * from books")
+    return jsonify(list(all_books))
