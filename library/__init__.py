64b50eff8723cfd309be17782295ff081bf4a3b0
Chiya <chiya@yandex.ru>
2022-10-08 14:19:42 +0300

    rework db

--- library/__init__.py
+++ library/__init__.py
@@ -5,7 +5,12 @@ from library.backend.db import create_connection
 app = Flask(__name__, static_folder="static/assets", template_folder="static")
 connection = create_connection("db.sqlite")
 cur = connection.cursor()
-cur.execute("create table if not exists books(id, title, link) ")
+cur.execute("""CREATE TABLE IF NOT EXISTS books(
+  id       INTEGER  NOT NULL  PRIMARY KEY  AUTO_INCREMENT,
+  title    VARCHAR(255),
+  author  VARCHAR(255),
+  year    INTEGER
+);""")
 
 from library.views import *
 from library.backend.api import *
--- library/backend/api.py
+++ library/backend/api.py
@@ -5,6 +5,9 @@ from flask import request, jsonify
 @app.route("/api/save_book")
 def api_save_book():
     requested_json = request.json
+    cur = create_connection("db.sqlite").cursor()
+    cur.execute("INSERT INTO tmp(title,author,year)")
+
 
 @app.route("/api/get_all_books")
 def api_get_all_books():
