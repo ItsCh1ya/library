7ad9133136b75d5cff556b8a04934d7c2b0f63d6
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-10-14 21:12:54 +0300

    database update

--- library/backend/api.py
+++ library/backend/api.py
@@ -1,5 +1,6 @@
 from library import app, db
 from flask import request, jsonify
+from bson.json_util import dumps
 
 @app.route("/api/save_book", methods=["POST", "PUT"])
 def api_save_book():
@@ -7,21 +8,25 @@ def api_save_book():
     db.books.insert_one({
         "title":requested_json['title'],
         "author":requested_json['author'],
+        "year":requested_json['year'],
         "url":requested_json['url']
     })
     return jsonify({"status":"success", "description":"Book successfuly added to db"})
 
 @app.route("/api/get_all_books")
 def api_get_all_books():
-    all_books = db.books.find({})
-    return jsonify(list(all_books))
+    lBooks = db.books.find({})
+    return dumps(lBooks)
 
 @app.route("/api/edit_book", methods=["POST"])
 def api_edit_book():
     requested_json = request.json
-    cur = create_connection("db.sqlite").cursor()
-    cur.execute(f"""
-        UPDATE books
-        SET title = '{requested_json['title']}', author= '{requested_json['author']}, year = {requested_json['year']}, id = {requested_json['id']}', url = {requested_json['url']}
-        WHERE id = {requested_json['id']};
-    """)
+    db.books.find_one_and_update({
+        "_id":{"$oid":requested_json['id']}
+    },{
+        "title":requested_json['title'],
+        "author":requested_json['author'],
+        "year":requested_json['year'],
+        "url":requested_json['url']
+    })
+    return "ok"
