0e3c54ead123ebe2d87dd6ffdfcaa7f95295cf61
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-10-14 21:23:49 +0300

    added del_book

--- library/backend/api.py
+++ library/backend/api.py
@@ -13,6 +13,14 @@ def api_save_book():
     })
     return jsonify({"status":"success", "description":"Book successfuly added to db"})
 
+@app.route("/api/del_book")
+def api_delete_book():
+    requested_json = request.json
+    db.books.find_one_and_delete({
+        "_id":{"$oid":requested_json['id']}
+    }
+    return jsonify({"status":"success", "description":"Book successfuly deleted from db"})
+
 @app.route("/api/get_all_books")
 def api_get_all_books():
     lBooks = db.books.find({})
@@ -29,4 +37,4 @@ def api_edit_book():
         "year":requested_json['year'],
         "url":requested_json['url']
     })
-    return "ok"
+    return jsonify({"status":"success", "description":"Book successfuly edited in db"})
