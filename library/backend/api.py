3a80c029961fe1d96e080a577a74a054a619dc30
Kirill <kivahnin@gmail.com>
2022-10-15 23:27:24 +0300

    feat: end backend and create frontend

--- library/backend/api.py
+++ library/backend/api.py
@@ -1,40 +1,41 @@
-from library import app, db
+from crypt import methods
+from library import app
+from library.backend.db import create_connection
 from flask import request, jsonify
-from bson.json_util import dumps
 
 @app.route("/api/save_book", methods=["POST", "PUT"])
 def api_save_book():
     requested_json = request.json
-    db.books.insert_one({
-        "title":requested_json['title'],
-        "author":requested_json['author'],
-        "year":requested_json['year'],
-        "url":requested_json['url']
-    })
+    connection = create_connection("db.sqlite")
+    cur = connection.cursor()
+    cur.execute(f"INSERT INTO books(title,author,year,url) VALUES (?, ?, ?, ?)", (requested_json['title'], requested_json['author'], requested_json['year'], requested_json['url']))
+    connection.commit()
     return jsonify({"status":"success", "description":"Book successfuly added to db"})
 
-@app.route("/api/del_book")
+@app.route("/api/delete_book", methods=["POST"])
 def api_delete_book():
     requested_json = request.json
-    db.books.find_one_and_delete({
-        "_id":{"$oid":requested_json['id']}
-    })
-    return jsonify({"status":"success", "description":"Book successfuly deleted from db"})
+    connection = create_connection("db.sqlite")
+    cur = connection.cursor()
+    cur.execute("DELETE FROM books WHERE id = ?;", (requested_json['id'], ))
+    connection.commit()
+    return jsonify({"status":"success", "description":"da"})
 
 @app.route("/api/get_all_books")
 def api_get_all_books():
-    lBooks = db.books.find({})
-    return dumps(lBooks)
+    cur = create_connection("db.sqlite").cursor()
+    all_books = cur.execute("select * from books")
+    return jsonify(list(all_books))
 
 @app.route("/api/edit_book", methods=["POST"])
 def api_edit_book():
     requested_json = request.json
-    db.books.find_one_and_update({
-        "_id":{"$oid":requested_json['id']}
-    },{
-        "title":requested_json['title'],
-        "author":requested_json['author'],
-        "year":requested_json['year'],
-        "url":requested_json['url']
-    })
-    return jsonify({"status":"success", "description":"Book successfuly edited in db"})
+    connection = create_connection("db.sqlite")
+    cur = connection.cursor()
+    cur.execute(f"""
+        UPDATE books
+        SET title = ?, author = ?, year = ?, url = ?
+        WHERE id = ?;
+    """, (requested_json['title'], requested_json['author'], requested_json['year'], requested_json['url'], requested_json['id']))
+    connection.commit()
+    return jsonify({"status":"success", "description":"da"})
