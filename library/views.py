f958effcf613da2e4c183979aef249e3381dff4a
Chiya <ch1ya@yandex.ru>
2022-10-21 23:50:56 +0300

    moved lib

--- library/views.py
+++ library/views.py
@@ -3,6 +3,10 @@ from flask import render_template
 
 @app.route('/')
 def view_index():
+    return render_template('mainPage.html')
+
+@app.route('/library')
+def view_library():
     return render_template('index.html')
 
 @app.route('/create')
