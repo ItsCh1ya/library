ed39801ca65a23b7f988d7af57a1ce39ef8c509d
Chiya <ch1ya@yandex.ru>
2022-10-21 21:04:20 +0300

    move client

--- client/src/libs/actionData/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './sendData';
-export * from './parceData';
--- client/src/libs/actionData/parceData.ts
+++
@@ -1,3 +0,0 @@
-export function getById(data: Array<any>, id: number) {
-  return data.filter((element: any) => element.id === id);
-}
